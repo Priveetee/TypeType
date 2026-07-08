84a45a6ce0298e946d8c4fc348d9c71ea0ac5f46
Priveetee <brahim.benyoucefpro@proton.me>
2026-07-08 14:33:45 +0200

    feat: add mse workspace core

---
+++ packages/mse/src/index.ts
@@ -0,0 +1,12 @@
+export type { PlaybackWindow, PlaybackWindowRequest } from "./playback-window";
+export type { TypeTypeMseSnapshot } from "./player-snapshot";
+export { TypeTypeMsePlayer } from "./type-type-mse-player";
+export type {
+  TrackKind,
+  TypeTypeMseConfig,
+  TypeTypeMseEvent,
+  TypeTypeMseEventType,
+  TypeTypeMseListener,
+  TypeTypeMseQuality,
+  TypeTypeMseState,
+} from "./types";
